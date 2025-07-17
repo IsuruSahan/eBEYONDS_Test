67177e9944f5be45a9f73e428d1788dc090db6fb
IsuruSahan <12isurukumarasiri@gmail.com>
2025-07-17 15:40:02 +0530

    design almost ok

--- components/intro.php
+++ components/intro.php
@@ -1,10 +1,6 @@
 <section id="intro" class="intro">
   <div class="container">
     <h2>MOVIE LIBRARY</h2>
-    <p>
-      Welcome to our Movie Library. Discover your favorite shows, search by name,
-      and save them to your personal list. Easily manage your collection with add
-      and remove features. Happy watching!
-    </p>
+    <p>This app allows you to search for TV shows using the TV Maze API and save your favorites for later viewing.</p>
   </div>
 </section>
