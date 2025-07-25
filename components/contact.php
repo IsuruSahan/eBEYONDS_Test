951bb19512fa03dc067907875961c7c72e6e4ad1
IsuruSahan <12isurukumarasiri@gmail.com>
2025-07-25 14:53:20 +0530

    ui changes

--- components/contact.php
+++ components/contact.php
@@ -16,12 +16,14 @@
         <div class="form-group">
           <textarea name="comments" placeholder="Comments *" required></textarea>
         </div>
-        <button type="submit">Submit</button>
+        <div class="form-actions">
+          <button type="submit">Submit</button>
+        </div>
       </form>
 
       <!-- Google Maps Embed -->
       <div class="map">
-       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.3811634179906!2d79.93785737404802!3d6.844826519347855!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae25069caa2f53b%3A0xe7eae3a8b1f1214d!2seBEYONDS%20eBusiness%20%26%20Digital%20Solutions!5e0!3m2!1sen!2slk!4v1752743933845!5m2!1sen!2slk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.3811634179906!2d79.93785737404802!3d6.844826519347855!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae25069caa2f53b%3A0xe7eae3a8b1f1214d!2seBEYONDS%20eBusiness%20%26%20Digital%20Solutions!5e0!3m2!1sen!2slk!4v1752743933845!5m2!1sen!2slk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
     </div>
   </div>
--- components/favorites.php
+++ components/favorites.php
@@ -2,34 +2,30 @@
   <div class="container">
     <div class="favorites-header">
       <h2>Collect Your Favorites</h2>
-      <input type="text" id="searchInput" placeholder="Search for a show..." />
+      <div class="search-container">
+        <input type="text" id="searchInput" placeholder="Search for a show..." />
+      </div>
     </div>
 
-
-
+ 
+    <hr class="section-divider" />
 
     <!-- Static Cards -->
     <div class="static-cards">
       <div class="card">
         <img src="assets/static1.jpg" alt="Static Show 1" />
         <h3>Batman Returns</h3>
-        <p>Lorem ipsum dolor sit amet, 
-consetetur sadipscing elitr, sed diam 
-nonumy eirmod tempor invidunt ut…</p>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut…</p>
       </div>
       <div class="card">
         <img src="assets/static2.jpg" alt="Static Show 2" />
         <h3>Wild Wild West</h3>
-        <p>Lorem ipsum dolor sit amet, 
-consetetur sadipscing elitr, sed diam 
-nonumy eirmod tempor invidunt ut…</p>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut…</p>
       </div>
       <div class="card">
         <img src="assets/static3.jpg" alt="Static Show 3" />
         <h3>The Amazing Spiderman</h3>
-        <p>Lorem ipsum dolor sit amet, 
-consetetur sadipscing elitr, sed diam 
-nonumy eirmod tempor invidunt ut…</p>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut…</p>
       </div>
     </div>
 
